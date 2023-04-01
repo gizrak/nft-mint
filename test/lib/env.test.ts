1cab373dc98fb9ab6847c1bcba597337b0935e99
Ted Hwang <doodyfree@gmail.com>
2023-04-02 01:35:08 +0900

    Remove unused env variables

--- test/lib/env.test.ts
+++ test/lib/env.test.ts
@@ -1,9 +1,6 @@
 import env from '../../src/lib/env';
 
 test('env should exist.', () => {
-  expect(env.get('ENV')).not.toBeNull();
-  expect(env.get('LOG_LEVEL')).not.toBeNull();
-
   expect(env.get('PINATA_API_KEY')).not.toBeNull();
   expect(env.get('PINATA_API_SECRET')).not.toBeNull();
 
